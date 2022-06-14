85fdee3b766b9657e116cea74662bae41e6d10cc
mike6321 <mike6321@naver.com>
2022-06-14 17:51:59 +0900

    [junwoo.choi] feat : TestContainer init

---
+++ src/main/java/com/choi/test_code/study/StudyStatus.java
@@ -0,0 +1,5 @@
+package com.choi.test_code.study;
+
+public enum StudyStatus {
+    DRAFT, OPENED, STARTED, ENDED
+}
---
+++ src/test/java/com/choi/test_code/study/StudyServiceTest.java
@@ -0,0 +1,72 @@
+package com.choi.test_code.study;
+
+import com.choi.test_code.domain.Member;
+import com.choi.test_code.domain.Study;
+import com.choi.test_code.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+class StudyServiceTest {
+
+    @Mock MemberService memberService;
+
+    @Autowired StudyRepository studyRepository;
+
+    @Test
+    void createNewStudy() {
+        // Given
+        StudyService studyService = new StudyService(memberService, studyRepository);
+        assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("keesun@email.com");
+
+        Study study = new Study(10, "테스트");
+
+        given(memberService.findById(1L)).willReturn(Optional.of(member));
+
+        // When
+        studyService.createNewStudy(1L, study);
+
+        // Then
+        assertEquals(1L, study.getOwnerId());
+        then(memberService).should(times(1)).notify(study);
+        then(memberService).shouldHaveNoMoreInteractions();
+    }
+
+    @DisplayName("다른 사용자가 볼 수 있도록 스터디를 공개한다.")
+    @Test
+    void openStudy() {
+        // Given
+        StudyService studyService = new StudyService(memberService, studyRepository);
+        Study study = new Study(10, "더 자바, 테스트");
+        assertNull(study.getOpenedDateTime());
+
+        // When
+        studyService.openStudy(study);
+
+        // Then
+        assertEquals(StudyStatus.OPENED, study.getStatus());
+        assertNotNull(study.getOpenedDateTime());
+        then(memberService).should().notify(study);
+    }
+
+}
