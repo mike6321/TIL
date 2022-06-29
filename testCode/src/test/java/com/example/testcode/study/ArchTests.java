1ec7ee65297b185d3e42b8c480dac4b2555f6421
mike6321 <mike6321@naver.com>
2022-06-29 18:32:45 +0900

    [junwoo.choi] feat : (testCode) ArchUnit JUnit 5 연동

---
+++ testCode/src/test/java/com/example/testcode/study/ArchTests.java
@@ -0,0 +1,47 @@
+package com.example.testcode.study;
+
+import com.example.testcode.TestCodeApplication;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+
+@AnalyzeClasses(packagesOf = TestCodeApplication.class)
+public class ArchTests {
+
+    @ArchTest
+    ArchRule domainPackageRule = classes()
+            .that()
+            .resideInAnyPackage("..domain..")
+            .should()
+            .onlyBeAccessed()
+            .byClassesThat()
+            .resideInAnyPackage("..study..", "..member..", "..domain..");
+
+    @ArchTest
+    ArchRule memberPackageRule = noClasses()
+            .that()
+            .resideInAnyPackage("..domain..")
+            .should()
+            .accessClassesThat()
+            .resideInAPackage("..member..");
+
+    @ArchTest
+    ArchRule studyPackageRule = noClasses()
+            .that()
+            .resideOutsideOfPackage("..study..")
+            .should()
+            .accessClassesThat()
+            .resideInAPackage("..study..");
+
+    @ArchTest
+    ArchRule freeOfCycles = slices()
+            .matching("..testcode.(*)..")
+            .should()
+            .beFreeOfCycles();
+
+
+}
