b9e1f1dca7cf448d7b10dccc7e53cd9ad6b3bcf4
mike6321 <mike6321@naver.com>
2022-07-13 14:01:12 +0900

    [junwoo.choi] feat: (jpa) project init

--- testCode/src/main/java/com/example/testcode/domain/Study.java
+++ testCode/src/main/java/com/example/testcode/domain/Study.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
@@ -14,7 +15,7 @@ import java.time.LocalDateTime;
 @Getter @Setter @NoArgsConstructor
 public class Study {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private StudyStatus status = StudyStatus.DRAFT;
     private int limitCount;
--- testCode/src/test/java/com/example/testcode/study/request_body_test/RequestDto.java
+++ testCode/src/test/java/com/example/testcode/study/request_body_test/RequestDto.java
@@ -3,10 +3,8 @@ package com.example.testcode.study.request_body_test;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-//@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class RequestDto {
