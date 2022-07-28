623a91353acad9904a9ad69108ebdd5a9e7730db
mike6321 <mike6321@naver.com>
2022-07-28 10:19:04 +0900

    [junwoo.choi] chore : (spring- 2.1) 프로퍼티 마이그레이터

---
+++ spring/src/main/java/com/example/version/V_2_1/property_migrater/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.version.V_2_1.property_migrater;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
