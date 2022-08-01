eccc051420c28657c92f7a8fbea5751a73d4c100
mike6321 <mike6321@naver.com>
2022-08-01 23:19:59 +0900

    [junwoo.choi] feat : (spring- transaction) 트랜잭션 AOP 주의 사항 - 프록시 내부 호출1

---
+++ spring/src/test/java/com/example/spring/transcation/apply/InternalCallV1Test.java
@@ -0,0 +1,67 @@
+package com.example.spring.transcation.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV1Test {
+
+    @Autowired
+    private CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class = {}", callService.getClass());
+    }
+
+    @Test
+    void internalCall() {
+        callService.internal();
+    }
+
+    @Test
+    void externalCall() {
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+
+        @Bean
+        public CallService callService() {
+            return new CallService();
+        }
+
+    }
+
+    @Slf4j
+    static class CallService {
+
+        public void external() {
+            log.info("call external");
+            printTransactionInfo();
+            internal();
+        }
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTransactionInfo();
+        }
+
+        private void printTransactionInfo() {
+            boolean transactionActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("transaction active = {}", transactionActive);
+            boolean currentTransactionReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("transaction readOnly = {}", currentTransactionReadOnly);
+        }
+
+    }
+}
