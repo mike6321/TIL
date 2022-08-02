18dd57c5b97097fc5cf41f7d2b006604f27a909e
mike6321 <mike6321@naver.com>
2022-08-02 14:10:50 +0900

    [junwoo.choi] feat : (java) Crushing Matrixes with Barriers

---
+++ java/src/main/java/com/example/java/multi_thread/section05/Example02.java
@@ -0,0 +1,73 @@
+package com.example.java.multi_thread.section05;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+@Slf4j
+public class Example02 {
+
+    /**
+     * {9, 8, 9, 8}
+     * =>
+     * {1 + 2 + 3 + 1} = 7
+     * {7 + 2, 7 + 1, 7 + 2, 7 + 1}
+     * */
+    private static int[][] array = {
+            {1, 2, 3, 1},
+            {2, 1, 2, 1},
+            {1, 2, 1, 3},
+            {1, 1, 1, 2}
+    };
+    private static int[][] outputArray = {
+            {1, 2, 3, 1},
+            {9, 8, 9, 8},
+            {35, 36, 35, 37},
+            {144, 144, 144, 145}
+    };
+    private static final CyclicBarrier cyclicBarrier = new CyclicBarrier(4, () -> log.info("The barrier was released!"));
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            Thread thread = new Thread(new WorkerThread(i));
+            thread.start();
+            threads.add(thread);
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        log.info("The final array : {}", Arrays.deepToString(array));
+    }
+
+    static class WorkerThread implements Runnable {
+
+        private final int columnId;
+
+        WorkerThread(int columnId) {
+            this.columnId = columnId;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 1; i < 4; i++) {
+                int S = 0;
+                for (int j = 0; j < 4; j++) {
+                    S = S + array[i - 1][j];
+                }
+                array[i][columnId] = array[i][columnId] + S;
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+}
