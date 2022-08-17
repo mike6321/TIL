b5229c9cef0cedacfaef9cd34a3eb841be741a32
mike6321 <mike6321@naver.com>
2022-08-17 21:50:00 +0900

    [junwoo.choi] feat : (spring- transaction) DataSource 예제2 - 커넥션 풀

--- spring/src/test/java/com/example/spring/connection/ConnectionTest.java
+++ spring/src/test/java/com/example/spring/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package com.example.spring.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -18,8 +19,8 @@ public class ConnectionTest {
     void driverManager() throws SQLException {
         Connection connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         Connection connection2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        System.out.println("connection1 = " + connection1 + ", " + "connection1 class = " + connection1.getClass());
-        System.out.println("connection2 = " + connection2 + ", " + "connection2 class = " + connection2.getClass());
+        log.info("connection1 = {} , connection1 class = {}", connection1, connection1.getClass());
+        log.info("connection2 = {} , connection2 class = {}", connection2, connection2.getClass());
     }
 
     @Test
@@ -27,6 +28,19 @@ public class ConnectionTest {
         // DriverManagerDataSource - 항상 새로운 커넥션 획득
         DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(2);
+        dataSource.setPoolName("MyPool");
+        useDataSource(dataSource);
+        Thread.sleep(1000);
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
