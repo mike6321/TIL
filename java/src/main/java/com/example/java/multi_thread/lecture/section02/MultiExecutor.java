1b306a409fb6ac428364bcf1f34ded2f682da7f1
mike6321 <mike6321@naver.com>
2022-10-04 20:54:43 +0900

    feat : (thread) 스레드 생성 - MultiExecutor

---
+++ java/src/main/java/com/example/java/multi_thread/lecture/section02/MultiExecutor.java
@@ -0,0 +1,29 @@
+package com.example.java.multi_thread.lecture.section02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiExecutor {
+
+    private List<Runnable> tasks;
+
+    public MultiExecutor(List<Runnable> tasks) {
+        // Complete your code here
+        this.tasks = tasks;
+    }
+
+    public void executeAll() {
+        // complete your code here
+        List<Thread> threads = new ArrayList<>();
+        for (Runnable task : tasks) {
+            Thread thread = new Thread(task);
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        
+    }
+
+}
