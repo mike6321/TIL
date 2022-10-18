10dab4cb4166e350978ef2ddb1e33ab8ce66b7e4
mike6321 <mike6321@naver.com>
2022-10-18 23:21:52 +0900

    feat : (thread) Reentrant의 읽기/쓰기 락 및 데이터베이스 구현

---
+++ java/src/main/java/com/example/java/multi_thread/lecture/section07/Main02.java
@@ -0,0 +1,122 @@
+package com.example.java.multi_thread.lecture.section07;
+
+import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Reading took 391 ms
+ * */
+public class Main02 {
+
+    private static final int HIGHEST_PRICE = 1000;
+
+    public static void main(String[] args) throws InterruptedException {
+        InventoryDatabase inventoryDatabase = new InventoryDatabase();
+        Random random = new Random();
+
+        for (int i = 0; i < 100000; i++) {
+            inventoryDatabase.addItem(random.nextInt(HIGHEST_PRICE));
+        }
+
+        Thread writerThread = new Thread(() -> {
+            while (true) {
+                inventoryDatabase.addItem(random.nextInt(HIGHEST_PRICE));
+                inventoryDatabase.removeItem(random.nextInt(HIGHEST_PRICE));
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                }
+            }
+        });
+
+        writerThread.setDaemon(true);
+        writerThread.start();
+
+        int numberOfReaderThreads = 7;
+        List<Thread> readerThreads = new ArrayList<>();
+        for (int readerIndex = 0; readerIndex < numberOfReaderThreads; readerIndex++) {
+            Thread reader = new Thread(() -> {
+                for (int i = 0; i < 100000; i++) {
+                    int upperBoundPrice = random.nextInt(HIGHEST_PRICE);
+                    int lowerBoundPrice = upperBoundPrice > 0 ? random.nextInt(upperBoundPrice) : 0;
+                    inventoryDatabase.getNumberOfItemsInPriceRanges(lowerBoundPrice, upperBoundPrice);
+                }
+            });
+
+            reader.setDaemon(true);
+            readerThreads.add(reader);
+        }
+
+        long startReadingTime = System.currentTimeMillis();
+        for (Thread readerThread : readerThreads) {
+            readerThread.start();
+        }
+        for (Thread readerThread : readerThreads) {
+            readerThread.join();
+        }
+        long endReadingTime = System.currentTimeMillis();
+        System.out.printf("Reading took %d ms%n", endReadingTime - startReadingTime);
+    }
+
+    public static class InventoryDatabase {
+
+        private TreeMap<Integer, Integer> priceToCountMap = new TreeMap<>();
+        private ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+        private Lock readLock = reentrantReadWriteLock.readLock();
+        private Lock writeLock = reentrantReadWriteLock.writeLock();
+
+        public int getNumberOfItemsInPriceRanges(int lowerBound, int upperBound) {
+            readLock.lock();
+            try {
+                Integer fromKey = priceToCountMap.ceilingKey(lowerBound);
+                Integer toKey = priceToCountMap.floorKey(upperBound);
+
+                if (fromKey == null || toKey == null) {
+                    return 0;
+                }
+
+                NavigableMap<Integer, Integer> rangeOfPrices = priceToCountMap.subMap(fromKey, true, toKey, true);
+
+                int sum = 0;
+                for (Integer numberOfItemsForPrice : rangeOfPrices.keySet()) {
+                    sum += numberOfItemsForPrice;
+                }
+
+                return sum;
+            } finally {
+                readLock.unlock();
+            }
+        }
+
+        public void addItem(int price) {
+            writeLock.lock();
+            try {
+                Integer numberOfItemsForPrice = priceToCountMap.get(price);
+                if (numberOfItemsForPrice == null) {
+                    priceToCountMap.put(price, 1);
+                } else {
+                    priceToCountMap.put(price, numberOfItemsForPrice + 1);
+                }
+            } finally {
+                writeLock.unlock();
+            }
+        }
+
+        public void removeItem(int price) {
+            writeLock.lock();
+            try {
+                Integer numberOfItemsForPrice = priceToCountMap.get(price);
+                if (numberOfItemsForPrice == null || numberOfItemsForPrice == 1) {
+                    priceToCountMap.remove(price);
+                } else {
+                    priceToCountMap.put(price, numberOfItemsForPrice - 1);
+                }
+            } finally {
+                writeLock.unlock();
+            }
+        }
+
+    }
+
+}
