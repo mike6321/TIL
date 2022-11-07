5ecd1c8f730b3e374a290ffaa4128cfafcd2e261
mike6321 <mike6321@naver.com>
2022-11-08 08:38:04 +0900

    feat : (jpa) proxy 객체 초기화 확인

--- jpa/src/main/java/com/example/jpa/service/ReviewService.java
+++ jpa/src/main/java/com/example/jpa/service/ReviewService.java
@@ -1,5 +1,6 @@
 package com.example.jpa.service;
 
+import com.example.jpa.entity.Menu;
 import com.example.jpa.entity.Restaurant;
 import com.example.jpa.model.dto.response.ReviewResponse;
 import com.example.jpa.model.mapper.ReviewResponseMapper;
@@ -8,16 +9,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnitUtil;
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
 
     private final RestaurantRepository restaurantRepository;
+    private final EntityManagerFactory entityManagerFactory;
 
     @Transactional
     public ReviewResponse getReview(Long id) {
         Restaurant restaurant = restaurantRepository.findById(id)
                 .orElseThrow();
+
+        PersistenceUnitUtil persistenceUnitUtil = entityManagerFactory.getPersistenceUnitUtil();
+        boolean isLoadedRestaurantMoreInformation = persistenceUnitUtil.isLoaded(restaurant.getRestaurantMoreInformation());
+        System.out.println("isLoadedRestaurantMoreInformation = " + isLoadedRestaurantMoreInformation);
+        Set<Menu> menus = restaurant.getMenus();
+        boolean isLoadedMenus = persistenceUnitUtil.isLoaded(restaurant.getMenus());
+        System.out.println("isLoadedMenus = " + isLoadedMenus);
 
         return ReviewResponseMapper.of(restaurant);
     }
