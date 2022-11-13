cd41bc2d587265ea0a1d2cbc1064e98650f9f91d
mike6321 <mike6321@naver.com>
2022-11-13 18:07:02 +0900

    feat : (aop) JDK 동적 프록시 - 소개

---
+++ aop/src/test/java/com/example/aop/jdkdynamic/code/AInterface.java
@@ -0,0 +1,7 @@
+package com.example.aop.jdkdynamic.code;
+
+public interface AInterface {
+
+    String call();
+
+}
---
+++ aop/src/test/java/com/example/aop/jdkdynamic/code/BInterface.java
@@ -0,0 +1,7 @@
+package com.example.aop.jdkdynamic.code;
+
+public interface BInterface {
+
+    String call();
+
+}
