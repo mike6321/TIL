d6921abcd7fa6361e43f7bdcc75e96964f3db11e
mike6321 <mike6321@naver.com>
2022-11-21 20:45:46 +0900

    feat : (thread) 동기 작업과 비동기 작업 조합

--- java/src/main/java/com/example/java/multi_thread/book/java8_in_action/v2/BestPriceFinder.java
+++ java/src/main/java/com/example/java/multi_thread/book/java8_in_action/v2/BestPriceFinder.java
@@ -2,7 +2,12 @@ package com.example.java.multi_thread.book.java8_in_action.v2;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BestPriceFinder {
 
@@ -11,6 +16,14 @@ public class BestPriceFinder {
                                                    new Shop("MyFavoriteShop"),
                                                    new Shop("BuyItAll"),
                                                    new Shop("BuyItAll2"));
+    private final Executor executor = Executors.newFixedThreadPool(shops.size(), new ThreadFactory() {
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread thread = new Thread(r);
+            thread.setDaemon(true);
+            return thread;
+        }
+    });
 
     public List<String> findPricesSequential(String product) {
         return shops.stream()
@@ -18,6 +31,27 @@ public class BestPriceFinder {
                 .map(Quote::parse)
                 .map(Discount::applyDiscount) // delay
                 .collect(Collectors.toList());
+    }
+
+    public List<String> findPricesFuture(String product) {
+        List<CompletableFuture<String>> priceFutures = findPricesStream(product)
+                .collect(Collectors.toList());
+
+        return priceFutures.stream()
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
+    }
+
+    private Stream<CompletableFuture<String>> findPricesStream(String product) {
+        return shops.stream()
+                /*****************************비동기 1*****************************/
+                .map(shop -> CompletableFuture.supplyAsync(() -> shop.getPrice(product), executor)) // I/O
+                .map(future -> future.thenApply(Quote::parse)) // 동기
+                /*****************************비동기 1*****************************/
+
+                /*****************************비동기 2*****************************/
+                .map(future -> future.thenCompose(quote -> CompletableFuture.supplyAsync(() -> Discount.applyDiscount(quote), executor))); // I/O
+                /*****************************비동기 2*****************************/
     }
 
 }
