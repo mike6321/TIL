8c04b5d713429a6ec10480e12b6b1b867888d04f
mike6321 <mike6321@naver.com>
2022-12-03 20:19:19 +0900

    feat : (Java) @FunctionalInterface

---
+++ java/src/main/java/com/example/java/functionalinterface/Mono.java
@@ -0,0 +1,11 @@
+package com.example.java.functionalinterface;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Mono {
+
+    private final String exchange;
+    private final String chain;
+
+}
