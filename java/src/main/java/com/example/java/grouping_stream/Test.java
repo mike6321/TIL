58bba551644fdece0bd6bf0d1f859fefd751a87b
mike6321 <mike6321@naver.com>
2022-12-29 22:45:26 +0900

    feat : (java) stream grouping test

---
+++ java/src/main/java/com/example/java/grouping_stream/Test.java
@@ -0,0 +1,14 @@
+package com.example.java.grouping_stream;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Test {
+
+    private Long bannerTypeId;
+    private Long vendorBannerId;
+    private Long vendorId;
+
+}
