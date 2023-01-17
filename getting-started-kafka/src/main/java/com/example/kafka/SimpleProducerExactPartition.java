0cae3a9f579cfbfb9ad232c7fec5b01f9f10a1f2
mike6321 <mike6321@naver.com>
2023-01-17 10:39:23 +0900

    feat : (kafka) 파티션 번호를 지정한 프로듀서 애플리케이션

---
+++ getting-started-kafka/src/main/java/com/example/kafka/SimpleProducerExactPartition.java
@@ -0,0 +1,33 @@
+package com.example.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+@Slf4j
+public class SimpleProducerExactPartition {
+
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+        int partitionNo = 0;
+        ProducerRecord<String, String> record01 = new ProducerRecord<>(TOPIC_NAME, partitionNo,  "Pangyo", "Pangyo");
+        producer.send(record01);
+        log.info("{}", record01);
+        producer.flush();
+        producer.close();
+    }
+
+}
