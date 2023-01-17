2aa3518861b8fb258065b1f8477acba7a0d9f384
mike6321 <mike6321@naver.com>
2023-01-17 20:53:23 +0900

    feat : (kafka) 커스텀 파티셔너 프로듀서 애플리케이션

---
+++ getting-started-kafka/src/main/java/com/example/kafka/SimpleProducerCustomPartitioner.java
@@ -0,0 +1,71 @@
+package com.example.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.InvalidRecordException;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.utils.Utils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+@Slf4j
+public class SimpleProducerCustomPartitioner {
+
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class);
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+        ProducerRecord<String, String> record01 = new ProducerRecord<>(TOPIC_NAME, "Pangyo", "Pangyo");
+        producer.send(record01);
+        log.info("{}", record01);
+        ProducerRecord<String, String> record02 = new ProducerRecord<>(TOPIC_NAME, "Busan", "Busan");
+        producer.send(record02);
+        log.info("{}", record02);
+        producer.flush();
+        producer.close();
+    }
+
+    static class CustomPartitioner implements Partitioner {
+
+        @Override
+        public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
+            if (keyBytes == null) {
+                throw new InvalidRecordException("Need message key");
+            }
+            if (key.equals("Pangyo")) {
+                return 0;
+            }
+
+            List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
+            int numPartitions = partitions.size();
+            return Utils.toPositive(Utils.murmur2(keyBytes)) % numPartitions;
+        }
+
+        @Override
+        public void close() {
+
+        }
+
+        @Override
+        public void configure(Map<String, ?> configs) {
+
+        }
+
+    }
+
+}
