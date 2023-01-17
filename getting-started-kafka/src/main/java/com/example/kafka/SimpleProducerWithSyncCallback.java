cd0b7ff747ef0de6725383811faed72ffbdd57bf
mike6321 <mike6321@naver.com>
2023-01-17 21:10:10 +0900

    feat : (kafka) 레코드 전송 결과를 확인하는 프로듀서 애플리케이션

---
+++ getting-started-kafka/src/main/java/com/example/kafka/SimpleProducerWithSyncCallback.java
@@ -0,0 +1,38 @@
+package com.example.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+@Slf4j
+public class SimpleProducerWithSyncCallback {
+
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "Pangyo", "Pangyo");
+        try {
+            RecordMetadata metadata = producer.send(record).get();
+            log.info(metadata.toString());
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            producer.flush();
+            producer.close();
+        }
+    }
+
+}
