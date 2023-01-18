911540732b7fa087656d7ae938b7885b5acb0ea4
mike6321 <mike6321@naver.com>
2023-01-18 21:09:42 +0900

    refactor : (kafka) SimpleConsumer

---
+++ getting-started-kafka/src/main/java/com/example/kafka/consumer/AbstractConsumer.java
@@ -0,0 +1,22 @@
+package com.example.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Properties;
+
+public abstract class AbstractConsumer {
+
+    protected final static String TOPIC_NAME = "test";
+    protected final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+    protected final static String GROUP_ID = "test-group";
+    protected final static Properties configs = new Properties();
+
+    static {
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    }
+
+}
