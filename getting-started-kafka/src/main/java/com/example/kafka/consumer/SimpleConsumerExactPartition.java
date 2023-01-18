51762291fcb5edb3f4a08cab8637c51bcdcebef6
mike6321 <mike6321@naver.com>
2023-01-18 22:32:46 +0900

    feat : (kafka) 파티션 할당 컨슈머 애플리케이션

---
+++ getting-started-kafka/src/main/java/com/example/kafka/consumer/SimpleConsumerExactPartition.java
@@ -0,0 +1,29 @@
+package com.example.kafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+
+import java.time.Duration;
+import java.util.Collections;
+
+@Slf4j
+public class SimpleConsumerExactPartition extends AbstractConsumer {
+
+    private final static int PARTITION_NUMBER = 0;
+
+    public static void main(String[] args) {
+        consumer = new KafkaConsumer<>(configs);
+        consumer.assign(Collections.singleton(new TopicPartition(TOPIC_NAME, PARTITION_NUMBER)));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<String, String> record : records) {
+                log.info("record:{}", record);
+            }
+        }
+    }
+
+}
