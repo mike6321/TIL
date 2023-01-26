3f0b01ef7214757f0e62831a60515d99505cdd16
mike6321 <mike6321@naver.com>
2023-01-26 11:12:56 +0900

    feat : (kafka) producer demo for debug

---
+++ getting-started-kafka/src/main/java/com/example/kafka/references/sender/Application.java
@@ -0,0 +1,20 @@
+package com.example.kafka.references.sender;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner(Sender sender) {
+        return args -> sender.send("test");
+    }
+
+}
---
+++ getting-started-kafka/src/main/java/com/example/kafka/references/sender/Sender.java
@@ -0,0 +1,20 @@
+package com.example.kafka.references.sender;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class Sender {
+
+    private final KafkaTemplate<Integer, String> kafkaTemplate;
+
+    public void send(final String payload) {
+        log.info("sending payload : {}", payload);
+        kafkaTemplate.send("helloworld", payload);
+    }
+
+}
