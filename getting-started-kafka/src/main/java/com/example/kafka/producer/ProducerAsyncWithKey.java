7d010a453356b88e6026a899b7f456cc70e3795a
mike6321 <mike6321@naver.com>
2023-02-23 08:57:44 +0900

    feat : (kafka) core - 피자 주문 시뮬레이션 Producer 구현: 피자 주문 Producer 구현

--- getting-started-kafka/src/main/java/com/example/kafka/producer/ProducerAsyncWithKey.java
+++ getting-started-kafka/src/main/java/com/example/kafka/producer/ProducerAsyncWithKey.java
@@ -39,11 +39,11 @@ public class ProducerAsyncWithKey {
                 }
             });
 
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        }
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
