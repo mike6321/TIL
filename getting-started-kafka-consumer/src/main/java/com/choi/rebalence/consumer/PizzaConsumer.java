0b4abf64f2ef0a90059812a04cc562d16988effa
mike6321 <mike6321@naver.com>
2023-03-02 07:58:48 +0900

    feat : (kafka) core - rebalance

---
+++ getting-started-kafka-consumer/src/main/java/com/choi/rebalence/consumer/PizzaConsumer.java
@@ -0,0 +1,58 @@
+package com.choi.rebalence.consumer;
+
+import com.choi.AbstractConsumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+
+public class PizzaConsumer extends AbstractConsumer {
+
+    public static final Logger log = LoggerFactory.getLogger(PizzaConsumer.class.getName());
+
+    public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new ShutdownThread());
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        configs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        consumer = new KafkaConsumer<>(configs);
+        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+                for (ConsumerRecord<String, String> record : records) {
+                    log.info("record key: {}, partition: {}, record offset: {}, record value: {}", record.key(), record.partition(), record.offset(), record.value());
+                }
+                consumer.commitSync();
+            }
+        } catch (WakeupException e) {
+            log.warn("Wakeup consumer");
+        } finally {
+            log.warn("Consumer close");
+            consumer.close();
+        }
+    }
+
+    static class ShutdownThread extends Thread {
+        @Override
+        public void run() {
+            log.info("Shutdown hook");
+            consumer.wakeup();
+
+            try {
+                Thread.currentThread().join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
