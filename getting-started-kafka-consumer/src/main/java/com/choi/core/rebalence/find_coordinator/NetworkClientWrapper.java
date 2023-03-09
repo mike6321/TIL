e9bbcdbe55611238c65b237299de6486869cea24
mike6321 <mike6321@naver.com>
2023-03-09 22:44:20 +0900

    feat : (kafka) core - GroupCoordinator 찾기

---
+++ getting-started-kafka-consumer/src/main/java/com/choi/core/rebalence/find_coordinator/NetworkClientWrapper.java
@@ -0,0 +1,32 @@
+package com.choi.core.rebalence.find_coordinator;
+
+import org.apache.kafka.clients.ClientResponse;
+import org.apache.kafka.common.utils.Time;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkClientWrapper {
+
+    private final Time time = Time.SYSTEM;
+
+    /**
+     * @see org.apache.kafka.clients.NetworkClient#poll(long, long)
+     * @see org.apache.kafka.common.requests.FindCoordinatorResponse
+     *
+     *
+     * */
+    public List<ClientResponse> poll(long timeout, long now) {
+        long updatedNow = this.time.milliseconds();
+        List<ClientResponse> responses = new ArrayList<>();
+
+        handleCompletedReceives(responses, updatedNow); // broker 로부터 받은 FIND_COORDINATOR response
+
+        return responses;
+    }
+
+    private void handleCompletedReceives(List<ClientResponse> responses, long now) {
+
+    }
+
+}
