7571f884be623cd3728a368eac75df29ade0e2f5
mike6321 <mike6321@naver.com>
2023-03-14 22:32:24 +0900

    feat : (kafka) core - RetrieveCommitOffset

---
+++ getting-started-kafka-consumer/src/main/java/com/choi/core/rebalence/d_offset/FetcherWrapper.java
@@ -0,0 +1,14 @@
+package com.choi.core.rebalence.d_offset;
+
+import org.apache.kafka.clients.consumer.internals.Fetcher;
+
+public class FetcherWrapper {
+
+    /**
+     * @see Fetcher#resetOffsetsIfNeeded()
+     * */
+    public void resetOffsetsIfNeeded() {
+
+    }
+
+}
---
+++ getting-started-kafka-consumer/src/main/java/com/choi/core/rebalence/d_offset/RetrieveCommitOffset.java
@@ -0,0 +1,34 @@
+package com.choi.core.rebalence.d_offset;
+
+import org.apache.kafka.clients.consumer.internals.ConsumerCoordinator;
+import org.apache.kafka.clients.consumer.internals.Fetcher;
+import org.apache.kafka.clients.consumer.internals.SubscriptionState;
+import org.apache.kafka.common.utils.Timer;
+
+public class RetrieveCommitOffset<K, V> {
+
+    private final Fetcher<K, V> fetcher;
+    private final ConsumerCoordinator coordinator;
+    private final SubscriptionState subscriptions;
+
+    public RetrieveCommitOffset(Fetcher<K, V> fetcher, ConsumerCoordinator coordinator, SubscriptionState subscriptions) {
+        this.fetcher = fetcher;
+        this.coordinator = coordinator;
+        this.subscriptions = subscriptions;
+    }
+
+    /**
+     * @see org.apache.kafka.clients.consumer.KafkaConsumer#updateFetchPositions(Timer)
+     * */
+    private boolean updateFetchPositions(final Timer timer) {
+
+        if (coordinator != null && !coordinator.refreshCommittedOffsetsIfNeeded(timer)) return false;
+
+        subscriptions.resetInitializingPositions();
+
+        fetcher.resetOffsetsIfNeeded();
+
+        return true;
+    }
+
+}
