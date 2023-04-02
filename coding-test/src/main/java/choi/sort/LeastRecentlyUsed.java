b962ca075fe6059a9f51a21e1360797e09115352
mike6321 <mike6321@naver.com>
2023-04-02 23:35:26 +0900

    feat : ready to coding test (정렬)

---
+++ coding-test/src/main/java/choi/sort/LeastRecentlyUsed.java
@@ -0,0 +1,47 @@
+package choi.sort;
+
+import java.util.Scanner;
+
+public class LeastRecentlyUsed {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int cacheSize = in.nextInt();
+        int loopNum = in.nextInt();
+        int[] cache = new int[cacheSize];
+
+        for (int i = 0; i < loopNum; i++) {
+            int targetNum = in.nextInt();
+            int j;
+            boolean hit = false;
+            for (j = 0; j < cacheSize; j++) {
+                // cache hit
+                if (cache[j] == targetNum) {
+                    hit = true;
+                    break;
+                }
+            }
+
+            if (hit) {
+                int temp = cache[j];
+                for (int k = j; k > 0; k--) {
+                    cache[k] = cache[k-1];
+                }
+
+                cache[0] = temp;
+            } else {
+                for (int k = j-1; k > 0; k--) {
+                    cache[k] = cache[k-1];
+                }
+                cache[0] = targetNum;
+            }
+
+        }
+
+        for (int i : cache) {
+            System.out.print(i + " ");
+        }
+
+    }
+
+}
