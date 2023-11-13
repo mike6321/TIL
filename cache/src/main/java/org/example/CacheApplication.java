0ede7908cfd1e34ae002382754527b1b2dc5c44a
mike6321 <mike6321@naver.com>
2023-11-13 22:07:44 +0900

    feat : (cache) cache 정보 확인

--- cache/src/main/java/org/example/CacheApplication.java
+++ cache/src/main/java/org/example/CacheApplication.java
@@ -1,10 +1,17 @@
 package org.example;
 
+import org.example.domain.Member;
 import org.example.service.CreateMemberService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.interceptor.SimpleKey;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentMap;
 
 @SpringBootApplication
 public class CacheApplication {
@@ -26,10 +33,19 @@ public class CacheApplication {
         cacheManager.getCacheNames()
                         .forEach(name -> System.out.println("cache name : " + name));
 
+        System.out.println("##########################################################");
+
+        Cache cache = cacheManager.getCache("account.members");
+        System.out.println("Cache implementation class : " + cache.getClass().getCanonicalName());
+
+        ConcurrentMapCache concurrentMapCache = ConcurrentMapCache.class.cast(cache);
+        ConcurrentMap<SimpleKey, List<Member>> map = ConcurrentMap.class.cast(concurrentMapCache.getNativeCache());
+
+        System.out.println("Map Size : " + map.size());
+        map.entrySet()
+                .forEach(simpleKeyListEntry -> System.out.println(simpleKeyListEntry.getKey() + " : " + simpleKeyListEntry.getValue()));
 
         System.out.println("############################# end #############################");
-
-
     }
 
 }
