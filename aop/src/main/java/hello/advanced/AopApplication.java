06f7b2406a4a82db114d2286bbe420b9e5216ea4
mike6321 <mike6321@naver.com>
2025-09-13 10:45:20 +0900

    (aop) add logging configuration and improve application setup

--- aop/src/main/java/hello/advanced/AopApplication.java
+++ aop/src/main/java/hello/advanced/AopApplication.java
@@ -1,10 +1,14 @@
 package hello.advanced;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@Slf4j
 @SpringBootApplication
 public class AopApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(AopApplication.class, args);
